49b32f49b4858c325c01148066ff8586df54009c
Sanjeev-Chakaravarthy <sanjeev.ms28122@gmail.com>
2025-02-12 22:45:00 +0530

    Added MongoDB schemas and sample data

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoute = require('./routes/userRoute')
 const app = express();
-const PORT = process.env.PORT || 5000;
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
 
 
-app.get("/ping", (req, res) => {
-  res.send("Pong! 🏓");
-});
+app.use(express.json())
+
+app.use("/api/users", userRoute)
+
+app.get('/', (req, res) => {
+  try {
+    return res.status(200).send("Backend is running Successfully...")
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+})
+
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  try {
+    connectDB()
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } catch (err) {
+    console.error("Server failed to start!", err.message)
+  }
+  
 });
