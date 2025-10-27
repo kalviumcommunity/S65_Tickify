9b8f5b1db635badb14077984e5a3ee003a8fe798
Sanjeev-M.S <sanjeev.ms28122@gmail.com>
2025-10-27 14:43:22 +0530

    Update API target to use environment variables
    Refactor API target configuration to use environment variables based on NODE_ENV.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+
+// Load environment variables
 dotenv.config();
+
+const target =
+  process.env.NODE_ENV === "production"
+    ? process.env.VITE_BASE_URI_PROD
+    : process.env.VITE_BASE_URI_DEV;
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_BASE_URI, // Use environment variable
+        target,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
